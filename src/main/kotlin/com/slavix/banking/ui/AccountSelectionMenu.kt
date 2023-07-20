5e10c48861c37fe2282599ec459a2427786b13a2
Viacheslav <slavixg409@gmail.com>
2023-07-21 02:09:47 +0300

    Implemented AccountSelectionMenu

--- src/main/kotlin/com/slavix/banking/ui/AccountSelectionMenu.kt
+++ src/main/kotlin/com/slavix/banking/ui/AccountSelectionMenu.kt
@@ -1,4 +1,38 @@
 package com.slavix.banking.ui
 
+import com.slavix.banking.model.AccountType
+import com.slavix.banking.util.Console
+import com.slavix.banking.util.InputValidator
+
 class AccountSelectionMenu {
+    private var accountType: AccountType? = null
+    fun displayMenu() {
+        var userChoice = 0
+
+        while(accountType == null){
+            Console.println("Choose an option (1, 2, or 3)")
+            val userChoiceString = Console.readLine()
+
+            if(InputValidator.validInteger(userChoiceString)){
+                userChoice = userChoiceString.toInt()
+                Console.println("The selected option is: $userChoice")
+
+                accountType = when(userChoice){
+                    1 -> AccountType.DebitAccount
+                    2 -> AccountType.CreditAccount
+                    3 -> AccountType.CheckingAccount
+                    else -> {
+                        continue
+                    }
+                }
+            }
+            else{
+                continue
+            }
+        }
+    }
+
+    fun getAccountType(): AccountType? {
+        return accountType
+    }
 }
