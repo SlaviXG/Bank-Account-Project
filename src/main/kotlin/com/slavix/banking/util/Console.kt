c7de80d35ad433a2a88058af156b904b2bd172f9
Viacheslav <slavixg409@gmail.com>
2023-07-21 01:16:47 +0300

    Added a wrapper class for console IO operations

--- src/main/kotlin/com/slavix/banking/util/Console.kt
+++ src/main/kotlin/com/slavix/banking/util/Console.kt
@@ -1,4 +1,19 @@
 package com.slavix.banking.util
 
-class Console {
+import java.util.*
+
+object Console {
+    private val scanner = Scanner(System.`in`)
+
+    fun readLine(): String {
+        return scanner.nextLine()
+    }
+
+    fun print(message: Any?){
+        kotlin.io.print(message)
+    }
+
+    fun println(message: Any?){
+        kotlin.io.println(message)
+    }
 }
