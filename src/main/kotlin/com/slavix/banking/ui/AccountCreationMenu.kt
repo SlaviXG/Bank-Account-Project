5e8c30833317899bcfa4acb395fa2a2ebfaf2c02
Viacheslav <slavixg409@gmail.com>
2023-07-21 02:25:15 +0300

    Started implementing account creation

--- src/main/kotlin/com/slavix/banking/ui/AccountCreationMenu.kt
+++ src/main/kotlin/com/slavix/banking/ui/AccountCreationMenu.kt
@@ -1,4 +1,14 @@
 package com.slavix.banking.ui
 
+import com.slavix.banking.ui.AccountSelectionMenu
+import com.slavix.banking.util.Console
+
 class AccountCreationMenu {
+    fun displayMenu(){
+        Console.println("Welcome to the account creation menu.")
+        Console.println("What type of account would you like to create?")
+
+        val accountSelectionMenu: AccountSelectionMenu = AccountSelectionMenu()
+        accountSelectionMenu.displayMenu()
+    }
 }
--- src/main/kotlin/com/slavix/banking/ui/Main.kt
+++ src/main/kotlin/com/slavix/banking/ui/Main.kt
@@ -1,2 +1,8 @@
 package com.slavix.banking.ui
 
+import com.slavix.banking.ui.AccountCreationMenu
+fun main() {
+
+    val accountCreationMenu = AccountCreationMenu()
+    accountCreationMenu.displayMenu()
+}
